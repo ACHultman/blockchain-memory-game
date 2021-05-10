7f6a11ec3fcfd02a98a266f44f6d09e5fe058f2e
ACHultman <ahultman0@gmail.com>
2021-05-09 21:31:52 -0700

    feat: add web3, blockchain to frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,38 @@
-import React, { Component } from 'react';
-import Web3 from 'web3'
-import './App.css';
-import MemoryToken from '../abis/MemoryToken.json'
-import brain from '../brain.png'
+import React, { Component } from "react";
+import Web3 from "web3";
+import "./App.css";
+import MemoryToken from "../abis/MemoryToken.json";
+import brain from "../brain.png";
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3();
+    this.loadBlockchainData();
+  }
 
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("Non-Ethereum browser detected.");
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+  }
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      account: '0x0'
-    }
+      account: "0x0",
+    };
   }
 
   render() {
@@ -24,12 +45,20 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-          <img src={brain} width="30" height="30" className="d-inline-block align-top" alt="" />
-          &nbsp; Memory Tokens
+            <img
+              src={brain}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt=""
+            />
+            &nbsp; Memory Tokens
           </a>
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-muted"><span id="account">{this.state.account}</span></small>
+              <small className="text-muted">
+                <span id="account">{this.state.account}</span>
+              </small>
             </li>
           </ul>
         </nav>
@@ -39,26 +68,14 @@ class App extends Component {
               <div className="content mr-auto ml-auto">
                 <h1 className="d-4">Edit this file in App.js!</h1>
 
-                <div className="grid mb-4" >
-
-                  {/* Code goes here... */}
-
-                </div>
+                <div className="grid mb-4">{/* Code goes here... */}</div>
 
                 <div>
-
                   {/* Code goes here... */}
 
-                  <div className="grid mb-4" >
-
-                    {/* Code goes here... */}
-
-                  </div>
-
+                  <div className="grid mb-4">{/* Code goes here... */}</div>
                 </div>
-
               </div>
-
             </main>
           </div>
         </div>
