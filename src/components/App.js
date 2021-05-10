787456c1042bba7eb2bfc1ac4a582d5cd35cb21d
ACHultman <ahultman0@gmail.com>
2021-05-09 21:58:41 -0700

    feat: add memory game logic

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,63 @@ import "./App.css";
 import MemoryToken from "../abis/MemoryToken.json";
 import brain from "../brain.png";
 
+const CARD_ARRAY = [
+  {
+    name: "fries",
+    img: "/images/fries.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "/images/cheeseburger.png",
+  },
+  {
+    name: "ice-cream",
+    img: "/images/ice-cream.png",
+  },
+  {
+    name: "pizza",
+    img: "/images/pizza.png",
+  },
+  {
+    name: "milkshake",
+    img: "/images/milkshake.png",
+  },
+  {
+    name: "hotdog",
+    img: "/images/hotdog.png",
+  },
+  {
+    name: "fries",
+    img: "/images/fries.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "/images/cheeseburger.png",
+  },
+  {
+    name: "ice-cream",
+    img: "/images/ice-cream.png",
+  },
+  {
+    name: "pizza",
+    img: "/images/pizza.png",
+  },
+  {
+    name: "milkshake",
+    img: "/images/milkshake.png",
+  },
+  {
+    name: "hotdog",
+    img: "/images/hotdog.png",
+  },
+];
+
 class App extends Component {
   async componentWillMount() {
     await this.loadWeb3();
     this.loadBlockchainData();
+    // load random-ordered cards
+    this.setState({ cardArray: CARD_ARRAY.sort(() => 0.5 - Math.random()) });
   }
 
   async loadWeb3() {
@@ -52,6 +105,56 @@ class App extends Component {
     }
   }
 
+  chooseImage = (cardId) => {
+    cardId = cardId.toString();
+    if (this.state.cardsWon.includes(cardId)) {
+      return window.location.origin + "/images/white.png";
+    } else if (this.state.cardsChosenId.includes(cardId)) {
+      return CARD_ARRAY[cardId].img;
+    } else {
+      return window.location.origin + "/images/blank.png";
+    }
+  };
+
+  flipCard = async (cardId) => {
+    let alreadyChosen = this.state.cardsChosen.length;
+
+    this.setState({
+      cardsChosen: [
+        ...this.state.cardsChosen,
+        this.state.cardArray[cardId].name,
+      ],
+      cardsChosenId: [...this.state.cardsChosenId, cardId],
+    });
+
+    if (alreadyChosen === 1) {
+      setTimeout(this.checkForMatch, 100);
+    }
+  };
+
+  checkForMatch = async () => {
+    const optionOneId = this.state.cardsChosenId[0];
+    const optionTwoId = this.state.cardsChosenId[1];
+    if (optionOneId === optionTwoId) {
+      alert("You clicked on the same image...!");
+    } else if (this.state.cardsChosen[0] === this.state.cardsChosen[1]) {
+      alert("You found the match!");
+      this.setState({
+        cardsWon: [...this.state.cardsWon, optionOneId, optionTwoId],
+        tokenURIs: [...this.state.tokenURIs, CARD_ARRAY[optionOneId].img],
+      });
+    } else {
+      alert("Sory, try again!");
+    }
+    this.setState({
+      cardsChosen: [],
+      cardsChosenId: [],
+    });
+    if (this.state.cardsWon.length === CARD_ARRAY.length) {
+      alert("Congratulations! You found them all!");
+    }
+  };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -59,6 +162,10 @@ class App extends Component {
       token: {},
       totalSupply: 0,
       tokenURIs: [],
+      cardArray: [],
+      cardsChosen: [],
+      cardsChosenId: [],
+      cardsWon: [],
     };
   }
 
@@ -95,7 +202,22 @@ class App extends Component {
               <div className="content mr-auto ml-auto">
                 <h1 className="d-4">Edit this file in App.js!</h1>
 
-                <div className="grid mb-4">{/* Code goes here... */}</div>
+                <div className="grid mb-4">
+                  {this.state.cardArray.map((card, key) => (
+                    <img
+                      alt={`${card.name}`}
+                      key={key}
+                      src={this.chooseImage(key)}
+                      data-id={key}
+                      onClick={(event) => {
+                        let cardId = event.target.getAttribute("data-id");
+                        if (!this.state.cardsWon.includes(cardId.toString())) {
+                          this.flipCard(cardId);
+                        }
+                      }}
+                    />
+                  ))}
+                </div>
 
                 <div>
                   {/* Code goes here... */}
