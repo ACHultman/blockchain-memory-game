b76d2d716c74e58265c189cde6b223ef33a5e938
ACHultman <ahultman0@gmail.com>
2021-05-09 21:15:50 -0700

    feat: add minting

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -6,5 +6,11 @@ contract MemoryToken is ERC721Full {
 
   constructor() ERC721Full("Memory Token", "MEMORY") public {}
 
+  function mint(address _to, string memory _tokenURI) public returns(bool) {
+    uint _tokenId = totalSupply().add(1);
+    _mint(_to, _tokenId);
+    _setTokenURI(_tokenId, _tokenURI);
+    return true;
+  }
 }
 
--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -31,4 +31,42 @@ contract("Memory Token", (accounts) => {
       assert.equal(symbol, "MEMORY");
     });
   });
+
+  describe("token dist", async () => {
+    let result;
+
+    it("mints token", async () => {
+      const actualTokenURI = "https://www.test-token-uri.com/test-nft";
+      await token.mint(accounts[0], actualTokenURI);
+      result = await token.totalSupply();
+      assert.equal(result.toString(), "1", "total supply is correct");
+
+      result = await token.balanceOf(accounts[0]);
+      assert.equal(result.toString(), "1", "balance is correct");
+
+      result = await token.ownerOf("1");
+      assert.equal(
+        result.toString(),
+        accounts[0].toString(),
+        "owner is correct"
+      );
+      result = await token.tokenOfOwnerByIndex(accounts[0], 0);
+
+      let balanceOf = await token.balanceOf(accounts[0]);
+      let tokenIds = [];
+      for (let i = 0; i < balanceOf; i++) {
+        let id = await token.tokenOfOwnerByIndex(accounts[0], i);
+        tokenIds.push(id.toString());
+      }
+      let expected = ["1"];
+      assert.equal(
+        tokenIds.toString(),
+        expected.toString(),
+        "tokenIds are correct"
+      );
+
+      let resultTokenURI = await token.tokenURI("1");
+      assert.equal(resultTokenURI, actualTokenURI);
+    });
+  });
 });
